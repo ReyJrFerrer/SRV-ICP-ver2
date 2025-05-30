f2135eb4b5eaae2eb185f0b3116711a629ea1b9b
ReyJrFerrer <2232722@slu.edu.ph>
2025-05-30 13:57:55 +0800

    removed old canister dependencies

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -7,14 +7,13 @@ import "@app/styles/provider.css";
 import { Client, InternetIdentity } from "@bundly/ares-core";
 import { IcpConnectContextProvider } from "@bundly/ares-react";
 
-import { candidCanisters } from "@app/canisters";
+
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const client = Client.create({
     agentConfig: {
       host: process.env.NEXT_PUBLIC_IC_HOST_URL!,
     },
-    candidCanisters,
     providers: [
       new InternetIdentity({
         providerUrl: process.env.NEXT_PUBLIC_INTERNET_IDENTITY_URL!,

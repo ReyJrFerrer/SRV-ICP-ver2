a4b7bb1c5c1e851c2df61137822fbb96a927a0e7
jadolation <zaratejandale15@gmail.com>
2025-06-01 19:49:30 +0800

    added the logo to the icon

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,14 +1,13 @@
 import type { AppProps } from "next/app";
+import Head from 'next/head'; 
 import "tailwindcss/tailwind.css";
-import "@app/styles/globals.css";
+import "@app/styles/globals.css"; 
 import "@app/styles/client-components.css";
 import "@app/styles/provider.css";
 import "react-datepicker/dist/react-datepicker.css";
 
 import { Client, InternetIdentity } from "@bundly/ares-core";
 import { IcpConnectContextProvider } from "@bundly/ares-react";
-
-
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const client = Client.create({
@@ -24,6 +23,10 @@ export default function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <IcpConnectContextProvider client={client}>
+      <Head>
+        <link rel="icon" href="/logo.svg" type="image/svg+xml" />
+    
+      </Head>
       <Component {...pageProps} />
     </IcpConnectContextProvider>
   );
